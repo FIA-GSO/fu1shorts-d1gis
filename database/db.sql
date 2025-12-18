6dee44d216c21949271f94dadce2dd5cc723355b
xdepRaven <buhr.patrick97@gmail.com>
2025-12-18 14:09:06 +0100

    inserts

--- database/db.sql
+++ database/db.sql
@@ -2,4 +2,9 @@ CREATE TABLE if not exists tables (
     number int primary key,
     seats int,
     is_Free boolean
-)
+);
+
+insert  into  tables values (1, 4, true), (2, 4, true), (3, 5, false);
+
+select * from tables
+where is_Free == true
