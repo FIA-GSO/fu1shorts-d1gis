545dc1ce05e92577563415af55eccdf953057640
Julian Grzanna <Julian.Grzanna@rewe-group.com>
2025-12-18 12:34:21 +0100

    miese api v2

--- api/api.py
+++ api/api.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
 
-api = FastAPI()
+app = FastAPI()
+
+@app.get("/tische")
+def get_freie_tische():
+    return "tisch"
+
+
+@app.post("/reservierungen")
+def post_reservierungen():
+    return "reservierungen"
+
+
+@app.get("/reservierungen")
+def get_reservierungen():
+    return "reservierungen"
+
+
+@app.delete("/reservierungen/{res_id}")
+def delete_reservierungen(res_id: int):
+    return "reservierungen"
