d7a75a2f4a19109544629fef96741759a9bb475b
Julian Grzanna <Julian.Grzanna@rewe-group.com>
2025-12-18 14:33:37 +0100

    reservations

--- database/db.sql
+++ database/db.sql
@@ -4,6 +4,15 @@ CREATE TABLE if not exists tables (
     is_Free boolean
 );
 
+create table if not exists reservations (
+    day DATE,
+    timeFrom DATE,
+    timeTo DATE,
+    tableNumber int foreign key,
+    id int primary key,
+    pin int,
+)
+
 insert  into  tables values (1, 4, true), (2, 4, true), (3, 5, false);
 
 select * from tables
